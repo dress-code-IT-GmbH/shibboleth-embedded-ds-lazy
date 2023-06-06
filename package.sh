ba6c9a1b2bd169f6decf7493bcc082d6ad6e41e3
Thomas W <thomaswar@users.noreply.github.com>
2023-06-06 15:46:58 +0200

    Update package.sh

--- package.sh
+++ package.sh
@@ -2,6 +2,8 @@
 
 mkdir idps_prepare
 cp idps_prepare.js idps_prepare
+cp LICENSE.txt idps_prepare
+cp README.md idps_prepare
 cp -a example idps_prepare
 tar -czf idps_prepare.tar.gz idps_prepare
 sha256sum idps_prepare.tar.gz > idps_prepare.tar.gz.sha256
